68c9992cde819bd1b0cc23d6f050382159028a7a
Lucas Andrade <lucas.alvandrade@gmail.com>
2023-05-18 06:19:09 -0300

    update

--- aula42.py
+++ aula42.py
@@ -1,7 +1,5 @@
 frase = 'aaaoooo'
-
 i = 0
-
 qtd_apareceu_mais_vezes = 0
 letra_apareceu_mais_vezes = ''
 
@@ -10,25 +8,18 @@ while i < len(frase):
     letra_atual = frase[i]
 
     if letra_atual == ' ':
-
         i += 1
-
         continue
-
+    
     qtd_atual = frase.count(letra_atual)
 
     if qtd_apareceu_mais_vezes < qtd_atual:
         qtd_apareceu_mais_vezes = qtd_atual
         letra_apareceu_mais_vezes = letra_atual
-
     i += 1
 
 print(
-
     'A letra que apareceu mais vezes foi '
-
     f'"{letra_apareceu_mais_vezes}" que apareceu '
-
     f'{qtd_apareceu_mais_vezes}x'
-
 )
