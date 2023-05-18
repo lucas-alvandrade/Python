be809bac28b85a32480e995e366b5a5b27df46f4
Lucas Andrade <lucas.alvandrade@gmail.com>
2023-05-18 05:30:48 -0300

    aulas adicionadas

--- aula42.py
+++ aula42.py
@@ -1,16 +1,13 @@
 frase = 'aaaoooo'
 
-
 i = 0
 
 qtd_apareceu_mais_vezes = 0
 letra_apareceu_mais_vezes = ''
 
-
 while i < len(frase):
 
     letra_atual = frase[i]
-
 
     if letra_atual == ' ':
 
@@ -20,14 +17,11 @@ while i < len(frase):
 
     qtd_atual = frase.count(letra_atual)
 
-
     if qtd_apareceu_mais_vezes < qtd_atual:
         qtd_apareceu_mais_vezes = qtd_atual
         letra_apareceu_mais_vezes = letra_atual
 
-
     i += 1
-
 
 print(
 
